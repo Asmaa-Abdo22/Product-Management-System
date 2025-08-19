3c19e60ec31bdff6e6603b234c077e432858bbe2
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2025-08-19 03:20:10 +0300

    complete project

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const descriptionInput = document.getElementById("description");
 const imageInput = document.getElementById("imageInput");
 const addBtn = document.getElementById("addBtn");
 const productsContainer = document.getElementById("productsContainer");
-
+const searchInput = document.getElementById("searchInput");
 // &----GLOBAL VARIABLES ----
 let productList;
 if (localStorage.getItem("productList") === null) {
@@ -14,7 +14,6 @@ if (localStorage.getItem("productList") === null) {
 } else {
   productList = JSON.parse(localStorage.getItem("productList"));
   displayAllProducts();
-  
 }
 // &----FUNCTIONS ----
 function addProduct() {
@@ -23,27 +22,28 @@ function addProduct() {
     category: categoryInput.value,
     price: priceInput.value,
     description: descriptionInput.value,
-    image: imageInput.files[0] ? imageInput.files[0].name : ""
+    image: imageInput.files[0] ? imageInput.files[0].name : "",
   };
+
   productList.push(product);
   localStorage.setItem("productList", JSON.stringify(productList));
   displayProduct(productList.length - 1);
   console.log(productList);
+  clearInputs();
 }
 function displayProduct(i) {
-
-let  productHTML = `
+  let productHTML = `
    <div class="p-1 item">
                      <div class=" rounded border border-1 p-3 border-mainn">
                         <img src="./imgs/${productList[i].image}" alt="${productList[i].name}" class="w-100">
                         <h4 class="mt-4">${productList[i].name}</h4>
                        <div class="d-flex justify-content-between align-items-center">
                          <h5>${productList[i].category}</h5>
-                        <h6>${productList[i].price}$</h6>
+                        <h6 class="text-success fw-bold"><span class="me-1 text-white">${productList[i].price}</span> $</h6>
                        </div>
-                        <p>${productList[i].description}</p>
+                        <p >${productList[i].description}</p>
                         <button class="btn btn-outline-warning me-2">Update</button>
-                 <button class="btn btn-outline-danger">Delete</button>
+                 <button class="btn btn-outline-danger" id="deleteBtn" onclick="deleteProduct(${i})">Delete</button>
                     </div>
                    </div>
                    
@@ -52,12 +52,41 @@ let  productHTML = `
   productsContainer.innerHTML += productHTML;
 }
 function displayAllProducts() {
-  productsContainer.innerHTML = "";
   for (let i = 0; i < productList.length; i++) {
     displayProduct(i);
+  }
+}
+
+function clearInputs() {
+  nameInput.value = "";
+  categoryInput.value = "";
+  priceInput.value = "";
+  descriptionInput.value = "";
+  imageInput.value = null;
+}
+function deleteProduct(index) {
+  productList.splice(index, 1);
+  localStorage.setItem("productList", JSON.stringify(productList));
+  productsContainer.innerHTML = "";
+  displayAllProducts();
+}
+function searchProduct() {
+  productsContainer.innerHTML = "";
+  for (let i = 0; i < productList.length; i++) {
+    if (
+      productList[i].name
+        .toLowerCase()
+        .includes(searchInput.value.toLowerCase())
+    ) {
+      displayProduct(i);
+    }
   }
 }
 // &----EVENTS ----
 addBtn.addEventListener("click", function () {
   addProduct();
+});
+
+searchInput.addEventListener("input", () => {
+  searchProduct();
 });
